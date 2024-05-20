e4291a4bf9a80ef2d2cae556d0fdb02feedc4b55
Rama-2020 <ramiroscerra@hotmail.com>
2024-05-20 22:59:23 +0200

    Added Search functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+
 import Palette from './components/Palette';
 import './App.css';
 
@@ -6,11 +8,28 @@ import palettes from './data/colorPalettes';
 
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPalettes = palettes.filter(palette =>
+    palette.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    palette.colors.some(color => color.toLowerCase().includes(searchTerm.toLowerCase()))
+  );
+
   return (
     <div className="App">
       <h1>Color Palettes</h1>
+      <input
+        type="text"
+        placeholder="Buscar paletas de colores..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
       <div className="palette-container">
-        {palettes.map(palette => (
+        {filteredPalettes.map(palette => (
           <Palette key={palette.id} palette={palette} />
         ))}
       </div>
