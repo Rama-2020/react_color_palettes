a3b12fd31a3346d50947b147cb8b6ccdb2a4e691
Rama-2020 <ramiroscerra@hotmail.com>
2024-05-20 19:29:38 +0200

    Show text on Hover mouse on each Color

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
 import './Color.css';
 
 function Color({ color }) {
+    const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
-    <div className="color" style={{ backgroundColor: color }}>
+    <div 
+    className="color" 
+    style={{ backgroundColor: color }}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
+    >
+        { isHovered ? <p>{color}</p> : null}
     </div>
   );
 }
