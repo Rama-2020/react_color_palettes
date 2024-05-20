b36b32af1b3e6d9b59c4d56815319e112157f287
Rama-2020 <ramiroscerra@hotmail.com>
2024-05-20 22:52:12 +0200

    Added copy of the color

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -13,12 +13,18 @@ function Color({ color }) {
     setIsHovered(false);
   };
 
+  const handleColorClick = () => {
+    navigator.clipboard.writeText(color);
+  };
+
+
   return (
     <div 
     className="color" 
     style={{ backgroundColor: color }}
     onMouseEnter={handleMouseEnter}
     onMouseLeave={handleMouseLeave}
+    onClick={handleColorClick}
     >
         {/* { isHovered ? <p>{color}</p> : null} */}
         <p>{color}</p>
